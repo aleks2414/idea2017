29401a501b4178ce9a59cef4f27d4d91a2210d21
aleks2414 <alejandroromo14@gmail.com>
2018-02-15 17:48:46 -0800

    seo

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,12 @@ class ContactsController < ApplicationController
 prepare_meta_tags(title: "Contacto | Idearia Lab Agencia de Innovación Latinoamericana",
   description: "Contáctanos! Generemos diferenciación, crecimiento y transformación a través de la innovación en tu organización.", 
   keywords: %w[Innovación en México, innovación en Guatemala, innovación en Perú, innovación en Latinoamérica, crecimiento de organizaciones, transformación de organizaciones, diferenciación.]
+          og: {
+        site_name: "Contacto | Idearia Lab",
+        title: "Contacto | Idearia Lab Agencia de Innovación Latinoamericana",
+        description: "Contáctanos! Generemos diferenciación, crecimiento y transformación a través de la innovación en tu organización.",
+        type: 'website'
+      }
 )
 
   end
